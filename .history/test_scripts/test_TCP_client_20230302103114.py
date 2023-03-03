000e882f3a128d74b07e649d4ef7d28157c683c5
wolf-spy <shixuan.shan@gmail.com>
2023-03-04 00:44:21 +0100

    add dtype_conversion func to test_tcp_client file

---
+++ .history/test_scripts/test_TCP_client_20230302103114.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2023/02/28 14:52:33
+@Author  :   Shixuan Shan 
+'''
+import socket
+from collections import defaultdict
+import struct as st
+
+# data types: 'str', 'int'(i), 'uint16'(H), 'uint32'(), 'float32', 'float64', 'hex'
+# big-endian encoded '>'
+
+############################### functions ###################################
+def socket_connect(TCP_IP = '127.0.0.1', PORT = 6501):
+    server_address = (TCP_IP, PORT)
+    sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk.connect(server_address)
+    return sk
+
+def socket_close(sk):
+    sk.close()
+
+def data_type_converter(original_type, target_type, data):
+    data_type_convert = defaultdict(lambda:
+                                    {
+                                    (original_fmt == 'hex'     and target_fmt == 'str'     ): 'a',
+                                    (original_fmt == 'hex'     and target_fmt == 'int'     ): 'a',
+                                    (original_fmt == 'hex'     and target_fmt == 'uint16'  ): 'a',
+                                    (original_fmt == 'hex'     and target_fmt == 'uint32'  ): 'a',
+                                    (original_fmt == 'hex'     and target_fmt == 'float32' ): 'a',
+                                    (original_fmt == 'hex'     and target_fmt == 'float64' ): 'a',
+                                    (original_fmt == 'str'     and target_fmt == 'hex'     ): 'a',
+                                    (original_fmt == 'int'     and target_fmt == 'hex'     ): 'a',
+                                    (original_fmt == 'uint16'  and target_fmt == 'hex'     ): 'a',
+                                    (original_fmt == 'uint32'  and target_fmt == 'hex'     ): 'a',
+                                    (original_fmt == 'float32' and target_fmt == 'hex'     ): 'a',
+                                    (original_fmt == 'float64' and target_fmt == 'hex'     ): 'a',
+                                    (Hx_mean_df.iloc[0, 1] == 'error2' or
+                                    Hx_mean_df.iloc[0, 2] == 'error2' or
+                                    Hx_mean_df.iloc[0, 3] == 'error2' or
+                                    Hx_mean_df.iloc[0, 4] == 'error2' or
+                                    Hx_mean_df.iloc[0, 5] == 'error2'): "JJ"
+                                    })
+    new_type = data_type_convert[original_type, target_type, data]
+    return new_type
+
+
+
+
+# data_type_converter()
+TCP_IP = '127.0.0.1'
+PORT = 6501 # avialable ports: 6501, 6502, 6503, 6504
+server_address = (TCP_IP, PORT)
+
+# create a TCP/IP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(server_address)
+
+s.close()
---
+++ .history/test_scripts/test_TCP_client_20230303214643.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2023/02/28 14:52:33
+@Author  :   Shixuan Shan 
+'''
+import socket
+from collections import defaultdict
+import struct as st
+from timeit import default_timer as timer
+
+# data types: 'str', 'int'(i), 'uint16'(H), 'uint32'(L), 'float32'(f), 'float64'(d), 'hex'
+# big-endian encoded '>'
+
+############################### functions #####################################
+#################### basic functions for creating commands ####################
+def socket_connect(TCP_IP = '127.0.0.1', PORT = 6501):
+    server_address = (TCP_IP, PORT)
+    sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sk.connect(server_address)
+    return sk
+
+def socket_close(sk):
+    sk.close()
+
+# only support formats in supported_formats list
+def data_type_converter(original_fmt, target_fmt, data):
+    supported_formats = ['int', 'uint16', 'uint32', 'float32', 'float64', 'hex', 'str']
+    if original_fmt in supported_formats and target_fmt in supported_formats:
+        try:
+            data_type_convert = defaultdict(lambda: None, 
+                                            {
+                                            # to hex
+                                            # (original_fmt == 'str'     and target_fmt == 'hex'     ): 'a',
+                                            ('int'    , 'hex'    ): lambda: st.pack('>i', data).hex(),
+                                            ('uint16' , 'hex'    ): lambda: st.pack('>H', data).hex(),
+                                            ('uint32' , 'hex'    ): lambda: st.pack('>L', data).hex(),
+                                            ('float32', 'hex'    ): lambda: st.pack('>f', data).hex(),
+                                            ('float64', 'hex'    ): lambda: st.pack('>d', data).hex(),
+                                            # from hex
+                                            ('hex'    , 'str'    ): lambda: bytes.fromhex(data).decode('utf-8'),
+                                            ('hex'    , 'int'    ): lambda: st.unpack('>i', bytes.fromhex(data))[0],
+                                            ('hex'    , 'uint16' ): lambda: st.unpack('>H', bytes.fromhex(data))[0],
+                                            ('hex'    , 'uint32' ): lambda: st.unpack('>L', bytes.fromhex(data))[0],
+                                            ('hex'    , 'float32'): lambda: st.unpack('>f', bytes.fromhex(data))[0],
+                                            ('hex'    , 'float64'): lambda: st.unpack('>d', bytes.fromhex(data))[0]
+                                            })
+            return data_type_convert[(original_fmt, target_fmt)]()
+        except ValueError:
+            print('ValueError: Check if the type of data matches with original_fmt')
+    else:
+        print("TyPeError: Please check the data types! Supported data formats are: 'int', 'uint16', 'uint32', 'float32', 'float64', 'hex', 'str'")
+
+
+# # data_type_converter()
+# TCP_IP = '127.0.0.1'
+# PORT = 6501 # avialable ports: 6501, 6502, 6503, 6504
+# server_address = (TCP_IP, PORT)
+
+# # create a TCP/IP socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(server_address)
+
+# s.close()
+original_fmt = 'int'
+target_fmt = 'hex'
+supported_formats = ['int', 'uint16', 'uint32', 'float32', 'float64', 'hex', 'str']
+data = 4.4
+print(data_type_converter(original_fmt, target_fmt, data))
